8a5b67f8f4eeb1c79657e0ed124742cea24f47f2
IlsevNierop <ilsevannierop@gmail.com>
2023-04-17 13:59:59 +0200

    lege push naar main

---
+++ src/main/java/com/example/huiswerktechiteasycontrollerspringboot/HuiswerkTechItEasyControllerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.huiswerktechiteasycontrollerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiswerkTechItEasyControllerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiswerkTechItEasyControllerSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/huiswerktechiteasycontrollerspringboot/HuiswerkTechItEasyControllerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huiswerktechiteasycontrollerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuiswerkTechItEasyControllerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
