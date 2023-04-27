3cf6242dd88a3d86a58dc9d1ca46dd0826a02925
IlsevNierop <ilsevannierop@gmail.com>
2023-04-27 14:36:48 +0200

    opzet

---
+++ src/main/java/com/example/huiswerktechiteasycontrollerspringboot/repositories/TelevisionRepository.java
@@ -0,0 +1,7 @@
+package com.example.huiswerktechiteasycontrollerspringboot.repositories;
+
+import com.example.huiswerktechiteasycontrollerspringboot.models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+}
